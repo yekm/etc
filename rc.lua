dc0c2f2a87b151ffc4583916e20f29ec5ce80286
Pavel V <src@yekm.name>
2017-02-21 20:43:31 +0300

    fixup! my minimal awesome config

--- rc.lua
+++ rc.lua
@@ -354,7 +354,7 @@ clientkeys = awful.util.table.join(
             c:raise()
         end,
         {description = "toggle fullscreen", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
+    awful.key({ modkey,           }, "c",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
